7d625ebdc8a2f83d8284ad934857e6a788c891eb
carlinbrr <carloscollaguazo5@gmail.com>
2025-08-25 13:26:23 +0200

    Api url modified to be the same one as the frontend - arleady have a reverse proxy config to redirect to backend thorugh container - not exposing ips

--- book-network-ui/src/app/services/api-configuration.ts
+++ book-network-ui/src/app/services/api-configuration.ts
@@ -11,7 +11,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://16.171.166.47:8088/api/v1';
+  rootUrl: string = 'http://16.171.166.47/api/v1';
 }
 
 /**
