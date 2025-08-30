931a282be964e55a4678733db411a894aa90b0f3
carlinbrr <carloscollaguazo5@gmail.com>
2025-08-30 17:44:10 +0200

    Modified interceptor to don't pass token for unsecured endpoints

--- book-network-ui/src/app/services/interceptor/http-token-interceptor.ts
+++ book-network-ui/src/app/services/interceptor/http-token-interceptor.ts
@@ -4,11 +4,17 @@ import {TokenService} from '../token/token.service';
 
 export const httpTokenInterceptor: HttpInterceptorFn = (req, next) => {
   const token = inject(TokenService).token;
+
+  if ( req.url.includes('authenticate') || req.url.includes('register') ) {
+    return next(req);
+  }
+
   if (token) {
     const authRequest = req.clone({
       headers: req.headers.append('Authorization', `Bearer ${token}`)
     });
     return next(authRequest);
   }
+
   return next(req);
 };
